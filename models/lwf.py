ff0862dd5d792ef04c6c2b35b685b7358060a45f
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-08-28 16:06:37 +0700

    Update lwf.py

--- models/lwf.py
+++ models/lwf.py
@@ -16,14 +16,14 @@ from torchvision import datasets, transforms
 from utils.autoaugment import CIFAR10Policy
 
 
-init_epoch = 200
+init_epoch = 20
 init_lr = 0.1
 init_milestones = [60, 120, 160]
 init_lr_decay = 0.1
 init_weight_decay = 0.0005
 
 # cifar100
-epochs = 100
+epochs = 20 
 lrate = 0.05
 milestones = [45, 90]
 lrate_decay = 0.1
@@ -374,7 +374,7 @@ class LwF(BaseLearner):
 
             data_iter = iter(train_loader)
 
-            for cycle in range(1):   # lặp 32 lần
+            for cycle in range(32):   # lặp 32 lần
                 # === 8 bước INNER ===
                 for _ in range(4):
                     try:
